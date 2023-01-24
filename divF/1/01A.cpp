d9b62531d82985febdec3cd06d2b466e326961e2
Igor Shavlov <108840149+HarryShavlov@users.noreply.github.com>
2023-01-24 11:43:50 +1000

    Update 01A.cpp

--- divF/1/01A.cpp
+++ divF/1/01A.cpp
@@ -1,1 +1,13 @@
-111
+Дано натуральное число. Найдите число десятков в его десятичной записи.
+  
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    cout << a/10%10 << std::endl;
+    return 0;
+}
